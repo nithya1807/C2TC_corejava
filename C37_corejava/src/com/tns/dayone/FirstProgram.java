3d556ac4d72abdee537d1dea98c7986279150d81
ELCOT <ELCOT@Lenovo>
2024-04-02 23:48:46 -0700

    First commit

---
+++ C37_corejava/src/com/tns/dayone/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.tns.dayone;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("first program");
+		System.out.println(" welcome all");
+	}
+
+}
